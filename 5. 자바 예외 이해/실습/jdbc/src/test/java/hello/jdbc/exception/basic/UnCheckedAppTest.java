5f5f0313cd197255c139467ea72367626edb9d2f
limlim <skyblue.777@daum.net>
2025-01-26 13:11:10 +0900

    [add] 자바 예외 이해(내용 정리)

--- 5. 자바 예외 이해/실습/jdbc/src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
+++ 5. 자바 예외 이해/실습/jdbc/src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
@@ -2,9 +2,11 @@ package hello.jdbc.exception.basic;
 
 import java.net.ConnectException;
 import java.sql.SQLException;
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+@Slf4j
 public class UnCheckedAppTest {
 
   @Test
@@ -12,6 +14,17 @@ public class UnCheckedAppTest {
     Controller controller = new Controller();
     Assertions.assertThatThrownBy(() -> controller.request())
         .isInstanceOf(RuntimeSQLException.class);
+  }
+
+  @Test
+  void printEx() {
+    Controller controller = new Controller();
+    try {
+      controller.request();
+    } catch (Exception e) {
+      //e.printStackTrace();
+      log.info("ex", e);
+    }
   }
 
 
@@ -59,6 +72,10 @@ public class UnCheckedAppTest {
   }
 
   static class RuntimeSQLException extends RuntimeException {
+
+    public RuntimeSQLException() {
+    }
+
     public RuntimeSQLException(Throwable cause) {
       super(cause);
     }
